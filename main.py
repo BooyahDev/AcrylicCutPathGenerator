31601fa1817e6166830e82b8baf91ae02c9c207c
mizofumi <mizofumi@booyah.dev>
2025-06-18 01:03:06 +0900

    fix cros

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from tempfile import NamedTemporaryFile
 import shutil
 import os
 from generate_cutpath import generate_cutpath
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+#    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/generate")
 async def create_svg(file: UploadFile = File(...), offset: int = Form(10)):
